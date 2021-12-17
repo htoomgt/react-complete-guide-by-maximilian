f3d8b0781bfc69d9aaa8d7d390172c859a7efd99
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-12-17 07:24:04 +0630

    80 using css modules

--- course-and-goals/src/components/UI/Button/Button.js
+++ course-and-goals/src/components/UI/Button/Button.js
@@ -1,5 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import styles from './Button.module.css';
+
+/* import styled from "styled-components";
 
 const Button = styled.button`
     width: 100%;
@@ -26,14 +28,14 @@ const Button = styled.button`
         border-color: #ac0e77;
         box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
     }
-`;
+`; */
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
