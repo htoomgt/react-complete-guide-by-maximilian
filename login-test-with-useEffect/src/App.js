c4eca374ac857a817744a91f8eec1dbd9a01f729
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-12-28 12:20:10 +0630

    112 use Effect and dependencies complete

--- login-test-with-useEffect/src/App.js
+++ login-test-with-useEffect/src/App.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLogin');
+
+    if(storedUserLoggedInInformation  === '1'){
+      setIsLoggedIn(true);
+    }
+   
+  }, []);
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLogin', '1');
     setIsLoggedIn(true);
+    
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLogin');
     setIsLoggedIn(false);
+    
   };
 
   return (
