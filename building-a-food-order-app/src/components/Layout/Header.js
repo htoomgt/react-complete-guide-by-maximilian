831a6418e0940d5cc8393a2256d8f05e21e6fd1e
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-12-31 10:16:21 +0630

    134 adding cart button component complete

--- building-a-food-order-app/src/components/Layout/Header.js
+++ building-a-food-order-app/src/components/Layout/Header.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classes from './Header.module.css';
-import mealsImage from '../../assets/images/meals.jpg';
+import { Fragment } from "react";
+import PropTypes from "prop-types";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/images/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = props => {
+const Header = (props) => {
     return (
-        <React.Fragment>
-            <header className={`${classes.header}`}>
-                <h1>React Meals</h1>
-                <button>Cart</button>
+        <Fragment>
+            <header className={classes.header}>
+                <div className={classes.headerContainer}>
+                    <h1>ReactMeals</h1>
+                    <HeaderCartButton />
+                </div>
             </header>
-            <div className={`${classes['main-image']}`}>
-                <img src={mealsImage} alt="A table full of delicious food" />
-            </div>      
-        </React.Fragment>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="A table full of delicious food!" />
+            </div>
+        </Fragment>
     );
-}
+};
 
-Header.propTypes = {
+Header.propTypes = {};
 
-}
-
-export default Header
+export default Header;
