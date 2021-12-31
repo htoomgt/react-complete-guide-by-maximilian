65c7e0c3973bc8157e409eb5def84ca500de500e
htoo-mg-thait <htoomaungthait@gmail.com>
2021-12-31 18:29:37 +0630

    148 make item removable complete

--- building-a-food-order-app/src/store/CartProvider.js
+++ building-a-food-order-app/src/store/CartProvider.js
@@ -1,89 +1,92 @@
-import React, {useReducer} from 'react';
-import PropTypes from 'prop-types';
-import CartContext from './cart-context';
+import React, { useReducer } from "react";
+import PropTypes from "prop-types";
+import CartContext from "./cart-context";
 
 const defatulCartState = {
-    items : [],
-    totalAmount : 0
-}
+    items: [],
+    totalAmount: 0,
+};
 
 const cartReducer = (state, action) => {
-    if(action.type === 'ADD'){
-        
-
+    if (action.type === "ADD") {
         const existingCartItemIndex = state.items.findIndex(
             (item) => item.id === action.item.id
-          );
+        );
         const existingCartItem = state.items[existingCartItemIndex];
-            
-
-        
 
         let updatedItem;
         let updatedItems = [];
 
-        if(existingCartItem){
+        if (existingCartItem) {
             updatedItem = {
                 ...existingCartItem,
                 amount: existingCartItem.amount + action.item.amount,
-              };
+            };
             updatedItems = [...state.items];
             updatedItems[existingCartItemIndex] = updatedItem;
-        }
-        else{            
-            updatedItems = state.items.concat(action.item);
-            console.log(updatedItems);
+        } else {
+            updatedItems = state.items.concat(action.item);           
         }
 
-        const updatedTotalAmount =  state.totalAmount + action.item.price * action.item.amount;
+        const updatedTotalAmount =
+            state.totalAmount + action.item.price * action.item.amount;
 
         return {
-            items : updatedItems,
-            totalAmount : updatedTotalAmount
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
         };
-        
-    }
-    else if(action.type === 'REMOVE'){
-        return state.items.splice(action.id, 1);
-    }
-    else{
+    } else if (action.type === "REMOVE") {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+          );
+          const existingItem = state.items[existingCartItemIndex];
+          const updatedTotalAmount = state.totalAmount - existingItem.price;
+          let updatedItems;
+          if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+          } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+          }
+      
+          return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+          };
+    } else {
         return defatulCartState;
     }
+};
 
-    
-}
-
-const CartProvider = props => {
-
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defatulCartState);
-
-
-
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(
+        cartReducer,
+        defatulCartState
+    );
 
     const addItemToCartHandler = (item) => {
-        dispatchCartAction({type: 'ADD', item: item});
-    }
+        dispatchCartAction({type : "ADD", item : item});
+    };
 
     const removeItemToCartHandler = (id) => {
-        dispatchCartAction({type: 'REMOVE', id: id});
-    }
+        dispatchCartAction({type : "REMOVE", id : id});
+    };
 
     const cartContext = {
         items: cartState.items,
-        totalAmount : cartState.totalAmount,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemToCartHandler
-    }
+        removeItem: removeItemToCartHandler,
+    };
 
     return (
         <CartContext.Provider value={cartContext}>
             {props.children}
         </CartContext.Provider>
-    )
-}
+    );
+};
 
-CartProvider.propTypes = {
+CartProvider.propTypes = {};
 
-}
-
-export default CartProvider
+export default CartProvider;
