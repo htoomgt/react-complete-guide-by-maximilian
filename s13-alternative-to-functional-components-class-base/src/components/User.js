2700a6080bedd87b652d4705466272b8dc5bf891
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-03 14:56:32 +0630

    adding first class based component

--- s13-alternative-to-functional-components-class-base/src/components/User.js
+++ s13-alternative-to-functional-components-class-base/src/components/User.js
@@ -1,7 +1,26 @@
 import classes from './User.module.css';
+import React, { Component } from 'react';
 
-const User = (props) => {
+
+/* const User = (props) => {
   return <li className={classes.user}>{props.name}</li>;
 };
 
+export default User; */
+
+
+
+
+class User extends Component {
+  
+
+  render() {
+    return (
+      <li className={classes.user}>{this.props.name}</li>
+    );
+  }
+}
+
 export default User;
+
+
