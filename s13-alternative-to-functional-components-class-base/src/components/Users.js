e3cb0a638e9de2d84dc3e3bc72a7d2506f53eb54
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-04 00:09:03 +0630

    working with state and events

--- s13-alternative-to-functional-components-class-base/src/components/Users.js
+++ s13-alternative-to-functional-components-class-base/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,7 +9,44 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
+class Users extends Component{
+  constructor() {
+    super();
+    this.state = {
+      // users: DUMMY_USERS,
+      showUsers: true,
+      more: 'Test'
+    };
+  }
+
+  toggleUsersHandler(){
+    this.setState( (curState) => {
+      return { showUsers : !curState.showUsers };
+    });
+  }
+
+  render(){
+
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+      <button onClick={this.toggleUsersHandler.bind(this)}>
+        {this.state.showUsers ? 'Hide' : 'Show'} Users
+      </button>
+      {this.state.showUsers && usersList}
+    </div>
+    );
+  }
+}
+
+/* const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {
@@ -33,5 +70,5 @@ const Users = () => {
     </div>
   );
 };
-
+ */
 export default Users;
