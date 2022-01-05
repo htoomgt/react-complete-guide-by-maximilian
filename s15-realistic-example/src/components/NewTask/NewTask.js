2c4b5ed6acc321cc9fef42752f20de731a4f6040
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-06 00:04:45 +0630

    195 using custom hooks in more components

--- s15-realistic-example/src/components/NewTask/NewTask.js
+++ s15-realistic-example/src/components/NewTask/NewTask.js
@@ -2,40 +2,44 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-    const [isLoading, setIsLoading] =  useState(false);
-    const [error, setError] = useState(null);
+    let {sendRequest:sendTaskRequest, isLoading , error} = useHttp();
+
+    let createTask = (taskText, taskData) => {
+        const generatedId = taskData.name;
+        const createdTask = { id : generatedId, text : taskText}
+        
+
+        props.onAddTask(createdTask);
+    }
+
+    
 
     const enterTaskHandler = async (taskText) => {
-        setIsLoading(true);
-        setError(null);
+        
 
-        try{        
-            const response =  await fetch('https://react-http-tutorial-backend-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json', {
+        
+            
+
+            sendTaskRequest({
+                url: 'https://react-http-tutorial-backend-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
                 method: 'POST',
-                body : JSON.stringify({ text :  taskText }),
                 headers: {
                     'Content-Type': 'application/json'
-                }  
-            });
+                },
+                body: { text: taskText },
+            }, createTask.bind(null, taskText));        
+            
 
-            if(!response.ok){
-                throw new Error('Request failed!');
-            }
-
-            const data =  await response.json();
-            const generatedId = data.name;
-            const createdTask = { id : generatedId, text : taskText}
-
-            props.onAddTask(createdTask);
+            
+            
 
 
-        }catch(error){
-            setError(error.message || 'Something went wrong!');
-        }
+        
 
-        setIsLoading(false);
+        
     }
 
 
