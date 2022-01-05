4ab2499efd0fa849fc33a9b8dc65aef2bfb90310
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-05 10:17:16 +0630

    181 useEffect for request http complete

--- s14-sending-http-request/src/App.js
+++ s14-sending-http-request/src/App.js
@@ -1,44 +1,68 @@
-import React from 'react';
-import './App.css';
-import MovieList from './components/MovieList';
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
+import MovieList from "./components/MovieList";
 
 function App() {
- const [movies, setMovies] = React.useState([]);
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  function fetchMovieHandler(){
-    fetch('https://swapi.dev/api/films/')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        const transFormedMovies = data.results.map(movie => {
-          return {
-            id : movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date
-          };
-        });
+    const fetchMovieHandler = useCallback(async () => {
+     
+        setIsLoading(true);
+        setError(null);
 
-        setMovies(transFormedMovies);
-      })
-      .catch();
-  }
+        try {
+            const response = await fetch("https://swapi.dev/api/films/");
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            const data = await response.json();
 
- 
+            const transFormedMovies = data.results.map((movie) => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date,
+                };
+            });
 
-
-  return (
+            setMovies(transFormedMovies);
+            setIsLoading(false);
+        } catch (error) {
+            setIsLoading(false);
+            setError(error.message);
+        }
     
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MovieList movies={movies}/>  
-      </section>
-    </React.Fragment>
-  );
+    }, []);
+
+    useEffect(() => {
+        fetchMovieHandler();
+    }, [fetchMovieHandler]);
+
+    let content = <h1>Found no Movies or no data to display</h1>;
+
+    if (isLoading) {
+        content = <h1>Loading...</h1>;
+    }
+
+    if (error) {
+        content = <h1>{error}</h1>;
+    }
+
+    if (movies.length > 0) {
+        content = <MovieList movies={movies} />;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMovieHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+        </React.Fragment>
+    );
 }
 
 export default App;
