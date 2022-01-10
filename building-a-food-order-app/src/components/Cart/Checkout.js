09da5cd014584f9f0319635d7ec94d100ec20607
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-10 15:11:48 +0630

    221 adding form validation

--- building-a-food-order-app/src/components/Cart/Checkout.js
+++ building-a-food-order-app/src/components/Cart/Checkout.js
@@ -1,8 +1,22 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import PropTypes from "prop-types";
 import classes from "./Checkout.module.css";
 
+const isEmpty = value => value.trim() === '';
+const isFiveChars = value => value.length === 5;
+
+
+
+
 const Checkout = (props) => {
+    const [formInputVality, setFormInputVality] = useState({
+        name : true,
+        street : true,
+        city : true,
+        postalCode: true
+    });
+
+
     const nameInputRef = useRef();
     const streetInputRef = useRef();
     const postalInputRef = useRef();
@@ -16,32 +30,87 @@ const Checkout = (props) => {
         const enteredPostalInput = nameInputRef.current.value;
         const enteredCityInput = nameInputRef.current.value;
 
+
+        const enteredNameIsValid = !isEmpty(enteredName);
+        const enteredStreetIsValid = !isEmpty(enteredStreetInput);
+        const enteredPostalIsValid = !isEmpty(enteredPostalInput);
+        const enteredCityIsValid = isFiveChars(enteredCityInput);
+
+        setFormInputVality({
+            name: enteredNameIsValid,
+            street: enteredStreetIsValid,
+            city: enteredCityIsValid,
+            postalCode : enteredPostalIsValid
+        });
+
+
+        const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredPostalIsValid && enteredCityIsValid;
+
+        if (formIsValid) {
+            // submtit cart form data
+
+        }
+            
         
+
 
 
     }   
 
+    const nameControlClasses = `
+        ${classes.control}
+        ${!formInputVality.name ? classes.invalid : ''}
+    `;
+
+    const streetControlClasses = `
+        ${classes.control}
+        ${!formInputVality.street ? classes.invalid : ''}
+    `;
+
+    const postalControlClasses = `
+        ${classes.control}
+        ${!formInputVality.postalCode ? classes.invalid : ''}
+    `;
+
+    const cityControlClasses = `
+        ${classes.control}
+        ${!formInputVality.city ? classes.invalid : ''}
+    `;
+
+
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
-            <div className={classes.control}>
+            <div 
+                className={nameControlClasses}
+            >
                 <label htmlFor="name"> Your Name </label>
                 <input type="text" id="name" ref={nameInputRef}/>
+                {!formInputVality.name && <p> Please enter a valid name </p>}
             </div>
 
-            <div className={classes.control}>
+            <div 
+                className={streetControlClasses}            
+            >
                 <label htmlFor="street"> Street </label>
                 <input type="text" id="street" ref={streetInputRef}/>
+                {!formInputVality.street && <p> Please enter a valid street </p>}
             </div>
 
-            <div className={classes.control}>
+            <div 
+                className={postalControlClasses}
+            >
                 <label htmlFor="postal"> Postal Code </label>
                 <input type="text" id="postal" ref={postalInputRef}/>
+                {!formInputVality.postalCode && <p> Please enter a valid postal code </p>}
             </div>
 
-            <div className={classes.control}>
+            <div 
+                className={cityControlClasses}
+            >
                 <label htmlFor="postal"> City </label>
                 <input type="text" id="city" ref={cityInputRef} />
+                {!formInputVality.city && <p> Please enter a valid city </p>}
             </div>
 
             <div className={classes.actions}>
