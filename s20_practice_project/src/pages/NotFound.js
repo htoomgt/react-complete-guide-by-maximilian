a7d497aaa997c4e350104c2645c31dfcb37d18ed
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-21 00:11:02 +0630

    283 adding not found page

---
+++ s20_practice_project/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <div className='centered'>
+      <p>
+        <h1>  Page not found!</h1>
+        </p>
+  </div>;
+};
+
+export default NotFound;
