d87220c7714850c121402c24dd13e67a719f61e9
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-20 22:56:48 +0630

    practice redirecting and extracting params

--- s20_practice_project/src/App.js
+++ s20_practice_project/src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Redirect} from 'react-router-dom';
 import AllQuotes from './pages/AllQuotes';
 import NewQuotes from './pages/NewQuotes';
 import QuoteDetail from './pages/QuoteDetail';
@@ -7,10 +7,13 @@ function App() {
   return (
     <div>
       <Switch >
+        <Route exact path="/" >
+          <Redirect to="quotes" />
+        </Route>
         <Route path="/quotes" exact >
           <AllQuotes />
         </Route>
-        <Route path="/quotes/:quotesId" >
+        <Route path="/quotes/:quoteId" >
           <QuoteDetail />    
         </Route>
         <Route path="/new-quote" exact >
--- s20_practice_project/src/pages/QuoteDetail.js
+++ s20_practice_project/src/pages/QuoteDetail.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const QuoteDetail = () => {
-  return <h1> Quote Detail Page</h1>;
+  const {quoteId} = useParams();
+
+  return (
+    <section>
+      <h1> Quote Detail Page</h1>
+      <p> Quote Id : {quoteId}</p>
+    </section>
+    
+  );
 };
 
 export default QuoteDetail;
