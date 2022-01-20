469f8c7db2704458941352e6480436af58c11dcb
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-20 22:45:17 +0630

    277 time to practice onwards to a new project

---
+++ s20_practice_project/src/App.js
@@ -0,0 +1,24 @@
+import { Switch, Route} from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import NewQuotes from './pages/NewQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+
+function App() {
+  return (
+    <div>
+      <Switch >
+        <Route path="/quotes" exact >
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quotesId" >
+          <QuoteDetail />    
+        </Route>
+        <Route path="/new-quote" exact >
+          <NewQuotes />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ s20_practice_project/src/pages/NewQuotes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewQuotes = () => {
+  return <h1> New Quotes </h1>;
+};
+
+export default NewQuotes;
