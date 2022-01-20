1de31403da4ea07613b5e6aae6298e1dc34c3dc6
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-20 10:49:40 +0630

    274 using switch and exact for configuring routes

--- s20_building_a_multipage_spa_with_react_router/src/App.js
+++ s20_building_a_multipage_spa_with_react_router/src/App.js
@@ -1,23 +1,31 @@
-import { Route } from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import MainHeader from './components/MainHeader';
+import { Route, Switch } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
-  return (
-    <div>
-      <MainHeader />
-      <main> 
-      <Route path="/welcome" > 
-        <Welcome />
-      </Route>
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Switch>
+                    <Route path="/welcome">
+                        <Welcome />
+                    </Route>
 
-      <Route path="/products" > 
-        <Products />
-      </Route>
-      </main>
-    </div>
-  );
+                    <Route path="/products"  exact>
+                        <Products />
+                    </Route>
+                    
+                    <Route path="/products/:productId"  >
+                        <ProductDetail />
+                    </Route>                   
+
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
 export default App;
