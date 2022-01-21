408a4abfbf9474114f3a5cf0fc5b757961093903
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-21 11:52:36 +0630

    286 working with query parameters

--- s20_practice_project/src/components/quotes/QuoteList.js
+++ s20_practice_project/src/components/quotes/QuoteList.js
@@ -1,23 +1,54 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+
+import { useHistory, useLocation }  from 'react-router-dom';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
-  return (
-    <Fragment>
-      <ul className={classes.list}>
-        {props.quotes.map((quote) => (
-          <QuoteItem
-            key={quote.id}
-            id={quote.id}
-            author={quote.author}
-            text={quote.text}
-          />
-        ))}
-      </ul>
-    </Fragment>
-  );
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+    const changeSortingHandler = () => {
+      console.log(queryParams.get('sort'));
+      history.push('/quotes?sort=' + (isSortingAscending ? 'dsc' : 'asc'));  
+      console.log(location);     
+    }
+
+    const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+
+    return (
+        <Fragment>
+            <div className={classes.sorting} >
+              <button onClick={changeSortingHandler} >Sort { isSortingAscending? `Acending` : `Descending` } </button>
+            </div>  
+                <ul className={classes.list}>
+                    {sortedQuotes.map((quote) => (
+                        <QuoteItem
+                            key={quote.id}
+                            id={quote.id}
+                            author={quote.author}
+                            text={quote.text}
+                        />
+                    ))}
+                </ul>
+            
+        </Fragment>
+    );
 };
 
 export default QuoteList;
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
