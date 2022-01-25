d761b0184682283f988a7815b77c8798bc6ee4d3
htoo-mg-thait <htoomaungthait@gmail.com>
2022-01-25 11:20:25 +0630

    290 adding the comments features

--- s20_practice_project/src/components/comments/NewCommentForm.js
+++ s20_practice_project/src/components/comments/NewCommentForm.js
@@ -1,12 +1,37 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 import classes from './NewCommentForm.module.css';
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+ 
+
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if(status === 'completed' && !error){
+      onAddedComment();
+    }
+
+  }, [status, error, onAddedComment])
 
   const submitFormHandler = (event) => {
     event.preventDefault();
+
+    const enteredText = commentTextRef.current.value;
+
+    sendRequest({
+      commentData : {text : enteredText},
+      quoteId : props.quoteId
+    });
+
+    
 
     // optional: Could validate here
 
@@ -15,6 +40,8 @@ const NewCommentForm = (props) => {
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && <div className={classes.centered}> <LoadingSpinner /></div>}
+
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
--- s20_practice_project/src/lib/api.js
+++ s20_practice_project/src/lib/api.js
@@ -55,15 +55,15 @@ export async function addQuote(quoteData) {
   return null;
 }
 
-export async function addComment(commentData, quoteId) {
-  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`, {
+export async function addComment(requestData) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${requestData.quoteId}.json`, {
     method: 'POST',
-    body: JSON.stringify(commentData),
+    body: JSON.stringify(requestData.commentData),
     headers: {
       'Content-Type': 'application/json',
     },
   });
-  const data = await response.json();
+  const data = await response.json(requestData.commentData);
 
   if (!response.ok) {
     throw new Error(data.message || 'Could not add comment.');
