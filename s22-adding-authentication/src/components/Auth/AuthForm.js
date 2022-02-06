e1749e7d9bbba3bdda8d105f5ad98e81bee2f6c3
htoo-mg-thait <htoomaungthait@gmail.com>
2022-02-06 19:08:11 +0630

    306 adding user login

--- s22-adding-authentication/src/components/Auth/AuthForm.js
+++ s22-adding-authentication/src/components/Auth/AuthForm.js
@@ -22,13 +22,18 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let urlToFetch = ``;
 
     // optional : Add validation
     if(isLogin){
+      urlToFetch = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${fireBaseAPIKey}`      
 
     }
     else{
-      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${fireBaseAPIKey}`, {
+      urlToFetch = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${fireBaseAPIKey}`      
+    }
+
+    fetch(urlToFetch, {
         method : 'POST',
         body : JSON.stringify({
           email : enteredEmail,
@@ -38,28 +43,27 @@ const AuthForm = () => {
         header: {
           'Content-Type' : 'applicaiton/json'
         }
-      }).then(res => {
+      }).then((res) => {
         setIsLoading(false);
-        if(res.ok){
-          // ...
-        }
-        else{
-          res.json().then((data) => {
-            // show an error modal 
-            let errorMessage = 'Authentication failed!';          
-            if(data && data.error.message){
-              errorMessage = data.error.message;
-            }
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
 
-            alert(errorMessage);
-
-
-
+            throw new Error(errorMessage);
           });
         }
       })
-        ;
-    }
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
 
   }
 
@@ -77,10 +81,10 @@ const AuthForm = () => {
           <input type='password' id='password' required  ref={passwordInputRef}/>
         </div>
         <div className={classes.actions}>
-          <button>
+          
           {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
           {isLoading && <p>Sending request...</p>}
-          </button>
+          
           <button
             type='button'
             className={classes.toggle}
