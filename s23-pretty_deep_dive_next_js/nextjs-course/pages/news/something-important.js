094730621df0c5cec483764b29bad7b446ee7a6f
htoo-mg-thait <htoomaungthait@gmail.com>
2022-02-13 21:12:47 +0630

    324 adding nested paths & pages (nested routes)

---
+++ s23-pretty_deep_dive_next_js/nextjs-course/pages/news/something-important.js
@@ -0,0 +1,11 @@
+// our-domain.com/news/something-important
+
+function DetailPage() {
+    return (
+        <>
+            <h1> Detail page</h1>
+        </>
+    );
+}
+
+export default DetailPage;
