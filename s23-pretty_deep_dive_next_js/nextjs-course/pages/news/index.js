bbbaba3434e84c94a091d848e5cbace126f70f8e
htoo-mg-thait <htoomaungthait@gmail.com>
2022-02-13 21:51:52 +0630

    327 linking between pages

--- s23-pretty_deep_dive_next_js/nextjs-course/pages/news/index.js
+++ s23-pretty_deep_dive_next_js/nextjs-course/pages/news/index.js
@@ -1,11 +1,24 @@
 // our-domain.com/news
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 
 
 function NewsPage() {
     return (
-        <>
+        <Fragment>
             <h1>The news Page</h1>
-        </>
+            <ul>
+                <li>
+                    <Link href="/news/next-js-is-a-great-framework"> NextJS is A Great Framework</Link>
+                </li>
+                <li> 
+                    <Link href="/news/next-js-is-good-to-study">NextJS is good to study. </Link></li>
+                <li> 
+                    <Link href="/news/js-is-great-too">JS is great too. </Link>
+                </li>
+            </ul>
+        </Fragment>
     );
 }
 
