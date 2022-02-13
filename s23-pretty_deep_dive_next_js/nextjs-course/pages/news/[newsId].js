eb7da9df1412c0e4e8af8cf20dd50c53e0aaaac4
htoo-mg-thait <htoomaungthait@gmail.com>
2022-02-13 21:36:44 +0630

    326 extracting dynamic parameter

--- s23-pretty_deep_dive_next_js/nextjs-course/pages/news/[newsId].js
+++ s23-pretty_deep_dive_next_js/nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,14 @@
 // our-domain.com/news/something-important
+import { useRouter } from 'next/router';
 
 function DetailPage() {
+    const router = useRouter();
+
+    console.log(router.query.newsId);
+
+    // send a request to the backend API here
+    // to fetch the news item with news Id
+
     return (
         <>
             <h1> Detail page</h1>
