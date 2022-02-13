116b346690e950bbbe7814d13bb8d8d55b519e9a
htoo-mg-thait <htoomaungthait@gmail.com>
2022-02-13 11:11:44 +0630

    313 adding auto logout

--- s22-adding-authentication/src/store/auth-context.js
+++ s22-adding-authentication/src/store/auth-context.js
@@ -9,6 +9,15 @@ const AuthContext = React.createContext({
     logout: () => {}
 });
 
+const calculateRemainingTime = (expirationTime) => {
+    const currentTime = new Date().getTime();
+    const adjExpirationTime  = new Date(expirationTime).getTime();
+
+    const remainingDuration  = adjExpirationTime - currentTime;
+    
+    return remainingDuration;
+}
+
 
 export const AuthContextProvider = (props) => {
     const initialToken = localStorage.getItem('token');
@@ -16,15 +25,21 @@ export const AuthContextProvider = (props) => {
 
     const userIsLoggedIn = !!token;
 
-    const loginHandler = (token) => {
-        setToken(token);
-        localStorage.setItem('token', token);
-    };
-
     const logoutHandler = () => {
         setToken(null);
         localStorage.removeItem('token');
     };
+    
+    const loginHandler = (token, expirationTime) => {
+        setToken(token);
+        localStorage.setItem('token', token);
+
+        const remainingTime = calculateRemainingTime(expirationTime);
+
+        setTimeout(logoutHandler, remainingTime);
+    };
+
+    
 
     const contextValue = {
         token: token,
