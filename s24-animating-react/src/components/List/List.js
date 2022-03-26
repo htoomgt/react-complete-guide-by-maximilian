33c78fae09022a2d5b1d291c5cfdaa2c0b214f5e
Htoo Maung Thait <hmthait@blueplanetmm.com>
2022-03-26 22:55:15 +0630

    363 aminating lists

--- s24-animating-react/src/components/List/List.js
+++ s24-animating-react/src/components/List/List.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import './List.css';
 
@@ -25,10 +27,13 @@ class List extends Component {
 
     render () {
         const listItems = this.state.items.map( (item, index) => (
-            <li 
+            <CSSTransition key={index} classNames="fade" timeout={300}>
+                <li 
                 key={index}
                 className="ListItem" 
                 onClick={() => this.removeItemHandler(index)}>{item}</li>
+            </CSSTransition>
+            
         ) );
 
         return (
@@ -36,7 +41,13 @@ class List extends Component {
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
                 <ul className="List">
-                    {listItems}
+                    <TransitionGroup 
+                        component="ul"
+                        className="List"
+                    >
+                        {listItems}
+                    </TransitionGroup>
+                    
                 </ul>
             </div>
         );
