e142a460b5d6ee87c714090a2bd037b44f390d2a
Htoo Maung Thait <hmthait@blueplanetmm.com>
2022-05-14 16:58:47 +0630

    389 grouping test together  with test suits

--- s26-testing-react-app/src/components/Greeting.test.js
+++ s26-testing-react-app/src/components/Greeting.test.js
@@ -1,19 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greetings.js';
 
-test('renders Hello World as text', ()=> {
-    // Arrange 
-    render(<Greeting />);
 
+describe('<Greeting component', () => {
 
-    // Act
-
-    //...nothing
-
-
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World', {exact: false});
-    expect(helloWorldElement).toBeInTheDocument();
-
-
+    test('renders Hello World as text', ()=> {
+        // Arrange 
+        render(<Greeting />);
+    
+    
+        // Act
+    
+        //...nothing
+    
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World', {exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    
+    
+    });
 });
+
+
