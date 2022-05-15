687383d0fbb9c5d76707de7c4c0be1f301b69e95
Htoo Maung Thait <hmthait@blueplanetmm.com>
2022-05-15 06:58:43 +0630

    395 testing react app

--- s26-testing-react-app/src/components/Async.test.js
+++ s26-testing-react-app/src/components/Async.test.js
@@ -3,6 +3,11 @@ import Async from './Async.js'
 
 describe('Async component', () => {
     test('renders posts if request succeds', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json : async () => [{ id : 'p1', title : 'First Post'}],
+        });
+
         render(<Async />);
 
         const listItemElements = await screen.findAllByRole('listitem');
