633c876b076f74d26b93e4de3823c26ae8f6b0b9
Htoo Maung Thait <hmthait@blueplanetmm.com>
2022-05-28 16:21:21 +0630

    410 adding a data model

--- s27-react-ts/src/App.tsx
+++ s27-react-ts/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todos';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ];
+
+
   return (
     <div> 
-      <Todos items={['Learn React', 'Learn Typescript']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
--- s27-react-ts/src/components/Todos.tsx
+++ s27-react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Todo from "../models/todos";
 
-const Todos: React.FC<{items: string[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]}> = (props) => {
   return (
     <ul>
       {props.items.map(item => {
-        return <li key={item}> {item} </li>
-      })}
+        return <li key={item.id}> {item.text} </li>
+      })}   
     </ul>
   );  
 }
