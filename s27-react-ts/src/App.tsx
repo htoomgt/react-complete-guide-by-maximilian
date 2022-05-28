34a12a5c9e6617b5de5b7bbc2e1d51ea149a231d
Htoo Maung Thait <hmthait@blueplanetmm.com>
2022-05-28 16:08:01 +0630

    409 working with props & type script

---
+++ s27-react-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div> 
+      <Todos items={['Learn React', 'Learn Typescript']}/>
+    </div>
+  );
+}
+
+export default App;
