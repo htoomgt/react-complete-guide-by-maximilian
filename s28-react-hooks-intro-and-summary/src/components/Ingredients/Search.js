fa4c99aa010d7d584b9907d8ad8bbf5bf3cd691a
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-09-11 15:42:41 +0630

    437 working with refs & useRef

--- s28-react-hooks-intro-and-summary/src/components/Ingredients/Search.js
+++ s28-react-hooks-intro-and-summary/src/components/Ingredients/Search.js
@@ -1,51 +1,61 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
-  const {onLoadingIngredients} = props;
-  const [enteredFilter, setEnteredFilter] = useState("");
+const Search = React.memo((props) => {
+    const { onLoadingIngredients } = props;
+    const [enteredFilter, setEnteredFilter] = useState("");
+    const inputRef = useRef();
 
-  useEffect(() => {
-    let timeoutId = setTimeout(() => {
-      const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    useEffect(() => {
+        let timeoutId = setTimeout(() => {
+            if (enteredFilter === inputRef.current.value) {
+                const query =
+                    enteredFilter.length === 0
+                        ? ""
+                        : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-      fetch('https://react-hooks-update-udemy-ceedb-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json'+query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadingIngredients = [];
-      for(const key in responseData){
-        loadingIngredients.push({id : key, title : responseData[key].title, amount : responseData[key].amount})
-      }
+                fetch(
+                    "https://react-hooks-update-udemy-ceedb-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json" +
+                        query
+                )
+                    .then((response) => response.json())
+                    .then((responseData) => {
+                        const loadingIngredients = [];
+                        for (const key in responseData) {
+                            loadingIngredients.push({
+                                id: key,
+                                title: responseData[key].title,
+                                amount: responseData[key].amount,
+                            });
+                        }
 
-      onLoadingIngredients(loadingIngredients);
-    })
+                        onLoadingIngredients(loadingIngredients);
+                    });
+            }
+        }, 400);
 
-   
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [enteredFilter, onLoadingIngredients, inputRef]);
 
-    }, 400);
-
-    return () =>{
-      clearTimeout(timeoutId);
-    }
-  }, [enteredFilter])
-
-
-  return (
-    <section className="search">
-      <Card>
-        <div className="search-input">
-          <label>Filter by Title</label>
-          <input 
-            type="text" 
-            value={enteredFilter}
-            onChange={(e) => setEnteredFilter(e.target.value)}
-          />
-        </div>
-      </Card>
-    </section>
-  );
+    return (
+        <section className="search">
+            <Card>
+                <div className="search-input">
+                    <label>Filter by Title</label>
+                    <input
+                        type="text"
+                        ref={inputRef}
+                        value={enteredFilter}
+                        onChange={(e) => setEnteredFilter(e.target.value)}
+                    />
+                </div>
+            </Card>
+        </section>
+    );
 });
 
-export default Search;  
+export default Search;
