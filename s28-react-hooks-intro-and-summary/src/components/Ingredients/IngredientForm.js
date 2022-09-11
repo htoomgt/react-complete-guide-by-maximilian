53bdcd5bccc969aee3515d01b2f10aaf642b989c
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-09-11 10:26:09 +0630

    427 more on useState & state updating

--- s28-react-hooks-intro-and-summary/src/components/Ingredients/IngredientForm.js
+++ s28-react-hooks-intro-and-summary/src/components/Ingredients/IngredientForm.js
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
-    event.preventDefault();
-    // ...
-  };
+const IngredientForm = React.memo((props) => {
+    const inputState = useState({ title: "", amount: "" });
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(inputState[0]);
+    };
+
+    return (
+        <section className="ingredient-form">
+            <Card>
+                <form onSubmit={submitHandler}>
+                    <div className="form-control">
+                        <label htmlFor="title">Name</label>
+                        <input
+                            type="text"
+                            id="title"
+                            value={inputState[0].title}
+                            onChange={(e) =>
+                                inputState[1]((prevState) => ({ ...prevState, title: e.target.value }))
+                            }
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="amount">Amount</label>
+                        <input
+                            type="number"
+                            id="amount"
+                            value={inputState[0].amount}
+                            onChange={(e) =>
+                                inputState[1]((prevState) => ({ ...prevState, amount: e.target.value }))
+                            }
+                        />
+                    </div>
+                    <div className="ingredient-form__actions">
+                        <button type="submit">Add Ingredient</button>
+                    </div>
+                </form>
+            </Card>
+        </section>
+    );
 });
 
 export default IngredientForm;
