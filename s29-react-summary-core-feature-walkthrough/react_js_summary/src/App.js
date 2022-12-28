3e00a8ee6fce046a107b7283d02b490f445df391
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-12-29 05:57:39 +0630

    463 building & re-using another component

--- s29-react-summary-core-feature-walkthrough/react_js_summary/src/App.js
+++ s29-react-summary-core-feature-walkthrough/react_js_summary/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello !</h1>
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
