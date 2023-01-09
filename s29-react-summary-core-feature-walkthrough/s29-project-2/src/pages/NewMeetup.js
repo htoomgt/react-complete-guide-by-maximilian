807bfb6050977dd2cb3c275e2020f9df2d6e4ccb
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-01-09 22:37:12 +0630

    477 adding a form

--- s29-react-summary-core-feature-walkthrough/s29-project-2/src/pages/NewMeetup.js
+++ s29-react-summary-core-feature-walkthrough/s29-project-2/src/pages/NewMeetup.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetupPage = () => {
   return (
-    <div>
-        <h1> NewMeetup</h1>
-    </div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
   )
 }
 
