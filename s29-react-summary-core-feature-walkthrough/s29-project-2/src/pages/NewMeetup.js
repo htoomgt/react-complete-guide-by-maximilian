fa5478adc5853002a89ba8f2429f63ab7d27a80d
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-01-10 23:36:54 +0630

    481 navigating programmatically

--- s29-react-summary-core-feature-walkthrough/s29-project-2/src/pages/NewMeetup.js
+++ s29-react-summary-core-feature-walkthrough/s29-project-2/src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetupPage = () => {
 
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData){
-    
+    fetch(
+      'https://react-getting-started-52357-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',          
+        }
+      }).then(() => {
+        navigate('/welcome', { replace : true})
+      });
   }
 
 
